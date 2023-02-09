83a5d4297f735592a7e785af2e3a243e3b64806b
William Moraes <willmoraes77@gmail.com>
2023-02-09 17:14:55 -0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data: function () {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar pedidos:</h1>
+      <Dashboard />
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from "../components/Dashboard";
+
+export default {
+  name: "Pedidos",
+  components: {
+    Dashboard,
+  },
+};
+</script>
+<style scoped></style>
